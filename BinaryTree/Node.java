d06ec1a7b7b0edb3212430e6bf28994205e9274f
Yitzhak Binyamin <yitzhakbin9@gmail.com>
2021-04-06 18:30:41 +0300

    Add files via upload

---
+++ BinaryTree/Node.java
@@ -0,0 +1,18 @@
+package BinaryTrees;
+
+public class Node {
+
+	
+	int data;
+	Node left , right;
+	
+	public Node(int d) {
+		this.data = d;
+		this.left = null;
+		this.right = null;
+	}
+	
+	
+	
+	
+}
